7f8d52196b91c55c984f741eb24488123228e2a5
dhiraj <Dhiraj.prasad578@gmail.com>
2023-09-22 17:20:30 +0530

    closing by QUIT()

--- dhiru23/src/test/java/dhiru/ranchi/india/TestGoogleWebSite.java
+++ dhiru23/src/test/java/dhiru/ranchi/india/TestGoogleWebSite.java
@@ -2,6 +2,7 @@ package dhiru.ranchi.india;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -9,13 +10,14 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class TestGoogleWebSite {
 	@Test
 	void launchgoogle() throws InterruptedException{
+
 		WebDriverManager.firefoxdriver().setup();
         WebDriver driver=new FirefoxDriver();
 		Thread.sleep(3000);
 		driver.get("https://www.google.com/");
 		driver.manage().window().maximize();
 		Thread.sleep(3000);
-		driver.close();
+		driver.quit();
 		
 	}
 
